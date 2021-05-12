365dee3345363a49586493c3dae0fc2de166d5a5
Rishabh <rishabhagarwal2005@gmail.com>
2021-05-12 12:48:05 +0530

    Coffee Machine

--- main.py
+++ main.py
@@ -1,19 +1,95 @@
-from data import question_data
-from question_model import Question
-from quiz_brain import QuizBrain
+from menu import MENU, resources
 
-question_bank = []
+nextAction = True
+# print(MENU)
+# print(resources)
 
-for question_data in question_data:
-    question_text = question_data['question']
-    answer = question_data['correct_answer']
-    question = Question(question_text, answer)
-    question_bank.append(question)
+report = {
+    'Water': resources['water'],
+    'Milk': resources['milk'],
+    'Coffee': resources['coffee'],
+    'Money': 0
+}
 
 
-quiz = QuizBrain(question_bank)
-while quiz.still_has_questions():
-    quiz.next_question()
+# def menu():
+#     print("==========================MENU=======================")
+#     print("Espresso :: $1.5")
+#     print("Latte :: $2.5")
+#     print("Cappuccino :: $3.0")
 
-print("You've completed the quiz")
-print(f"Your final score is :: {quiz.score}/{quiz.question_number}")
+
+def manageReport(coffee):
+    ingredients = coffee['ingredients']
+    cost = coffee['cost']
+    if report['Water'] >= ingredients['water']:
+        report['Water'] = report['Water'] - ingredients['water']
+        if coffee == 'latte' or coffee == 'cappuccino':
+            report['Milk'] = report['Milk'] - ingredients['milk']
+        report['Coffee'] = report['Coffee'] - ingredients['coffee']
+        report['Money'] = report['Money'] + cost
+        return True
+
+
+def checkAvailability(coffee):
+    isSufficient = False
+    ingredients = coffee['ingredients']
+    if report['Water'] >= ingredients['water'] and report['Coffee'] >= ingredients['coffee']:
+        if coffee != 'espresso':
+            if report['Milk'] >= ingredients['milk']:
+                isSufficient = True
+            else:
+                print("Sorry there is not enough milk")
+        else:
+            isSufficient = True
+    elif report['Water'] < ingredients['water']:
+        print("Sorry there is not enough water")
+    elif report['Coffee'] < ingredients['coffee']:
+        print("Sorry there is not enough coffee")
+
+# OR
+#     for item in report:
+#         if report[item] >= ingredients[item]:
+#             print(f"Sorry there is not enough {item}")
+#             isSufficient = False
+    return isSufficient
+
+
+def calculateMoney():
+    pennies = int(input("Enter number of pennies : "))
+    nickles = int(input("Enter number of nickles : "))
+    dimes = int(input("Enter number of dimes : "))
+    quarters = int(input("Enter number of quarters : "))
+    dollars = (pennies * 0.01 + nickles * 0.05 + dimes * 0.10 + quarters * 0.25)
+    return dollars
+
+
+def checkMoney(coffeePrice, customerMoney):
+    if customerMoney >= coffeePrice:
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+
+
+while nextAction:
+    customerChoice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if customerChoice == 'report':
+        print(report)
+    elif customerChoice == 'off':
+        nextAction = False
+    elif customerChoice == 'espresso' or customerChoice == 'latte' or customerChoice == 'cappuccino':
+        print(MENU[customerChoice]['cost'])
+        money = calculateMoney()
+        available = checkAvailability(MENU[customerChoice])
+        if not available:
+            nextAction = False
+        moneyStatus = checkMoney(MENU[customerChoice]['cost'], money)
+        if available and moneyStatus:
+            manageReport(MENU[customerChoice])
+            change = money - MENU[customerChoice]['cost']
+            if change > 0:
+                print(f"Here is ${round(change, 3)} dollars in change. Here is your drink {customerChoice} enjoy.")
+            else:
+                print(f"Here is your drink {customerChoice} enjoy.")
+
+# menu()
