cfe2185ff9204dc03328794f420dcc984f615526
Rishabh <rishabhagarwal2005@gmail.com>
2021-05-16 13:24:48 +0530

    Flash Card Game

--- main.py
+++ main.py
@@ -1,148 +1,70 @@
 from tkinter import *
-from tkinter import messagebox
+import pandas
 import random
-import pyperclip
-import json
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
 
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
-def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-    nr_letters = random.randint(8, 10)
-    nr_symbols = random.randint(2, 4)
-    nr_numbers = random.randint(2, 4)
-
-    password_letters = [random.choice(letters) for _ in range(nr_letters)]
-    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
-    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
-
-    password_list = password_letters + password_symbols + password_numbers
-
-    random.shuffle(password_list)
-    # global randomPassword
-    # for num in range(nr_letters):
-    #     randomPassword = randomPassword + random.choice(letters)
-    # for num in range(nr_symbols):
-    #     randomPassword = randomPassword + random.choice(symbols)
-    # for num in range(nr_numbers):
-    #     randomPassword = randomPassword + random.choice(numbers)
-    # randomPassword = ''.join(random.sample(randomPassword, len(randomPassword)))
-    # for char in password_list:
-    #     randomPassword = randomPassword + char
-    randomPassword = ''.join(password_list)
-    pyperclip.copy(randomPassword)
-    password_entry.insert(0, randomPassword)
-    # print(randomPassword)
+def generate_random_word():
+    # word = random.choice(list(english_french.items()))
+    # canvas.itemconfig(converted_language, text=word[1])
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(canvas_background, image=front_img)
+    canvas.itemconfig(language, text='French', fill='black')
+    canvas.itemconfig(converted_language, text=current_card['French'], fill='black')
+    flip_timer = window.after(3000, func=flip_card)
 
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+def flip_card():
+    canvas.itemconfig(canvas_background, image=back_image)
+    canvas.itemconfig(language, text='English', fill='white')
+    canvas.itemconfig(converted_language, text=current_card['English'], fill='white')
 
 
-def save():
-    website = website_entry.get()
-    email = email_entry.get()
-    password = password_entry.get()
-    user_data = {
-        website: {
-            'email': email,
-            'password': password
-        }
-    }
-
-    if len(website) > 0 and len(password) > 0:
-        # user_data = f'{website} | {email} | {password}'
-        # valid = messagebox.askokcancel(title=website, message=f'These are details entered: \n{user_data}')
-        # if valid:
-        # with open('user data.txt', mode='a') as file:
-        #     file.write(user_data + '\n')
-
-        try:
-            # Reading old data
-            with open('user_data.json', 'r') as file:
-                data = json.load(file)
-        except FileNotFoundError:
-            with open('user_data.json', 'w') as data_file:
-                json.dump(user_data, data_file, indent=4)
-        else:
-            # Updating old data with new data
-            data.update(user_data)
-            with open('user_data.json', 'w') as data_file:
-                json.dump(data, data_file, indent=4)
-        finally:
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
-
-    else:
-        messagebox.showinfo(title='oops', message='Do not leave any field empty')
+def known():
+    to_learn.remove(current_card)
+    words_to_learn = pandas.DataFrame(to_learn)
+    words_to_learn.to_csv('./data/words_to_learn.csv', index=False)
+    generate_random_word()
 
 
-def find_password():
-    try:
-        with open('user_data.json', 'r') as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        messagebox.showwarning(title='Error', message='No such data file found')
-    else:
-        website = website_entry.get()
-        if website in data:
-            web_detail = data[website]
-            email_entry.delete(0, END)
-            email_entry.insert(0, web_detail['email'])
-            password_entry.delete(0, END)
-            password_entry.insert(0, web_detail['password'])
-            messagebox.showinfo(title=website, message=f'{web_detail}')
-            print(data[website])
-        else:
-            password_entry.delete(0, END)
-            messagebox.showinfo(title='Error', message=f'No details for the {website} exists')
-
-
-# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-window.title('PASSWORD MANAGER')
-window.config(padx=50, pady=50)
+window.title('Flashy')
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
+
+try:
+    data = pandas.read_csv('./data/words_to_learn.csv')
+except FileNotFoundError:
+    original_data = pandas.read_csv('./data/french_words.csv')
+    to_learn = original_data.to_dict(orient='records')
+else:
+    to_learn = data.to_dict(orient='records')
+# english_french = {row.English: row.French for(index, row) in data.iterrows()}
+
 
 # Create Canvas
-canvas = Canvas(width=200, height=200)
-img = PhotoImage(file='logo.png')
-canvas.create_image(100, 100, image=img)
-canvas.grid(column=1, row=0)
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+# Get Image
+front_img = PhotoImage(file='./images/card_front.png')
+back_image = PhotoImage(file='./images/card_back.png')
+# Create Image
+canvas_background = canvas.create_image(400, 263, image=front_img)
+language = canvas.create_text(400, 150, text="", fill='black', font=('Ariel', 40, 'italic'))
+converted_language = canvas.create_text(400, 263, text="", fill='black', font=('Ariel', 60, 'bold'))
+canvas.grid(row=0, column=0, columnspan=2)
 
-# Creating Labels
-website_label = Label(text="Website :")
-website_label.grid(column=0, row=1)
+# Button
+wrong_img = PhotoImage(file='./images/wrong.png')
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=generate_random_word)
+wrong_button.grid(row=1, column=0)
 
-email_label = Label(text="Email/Username :")
-email_label.grid(column=0, row=2)
+right_img = PhotoImage(file='./images/right.png')
+right_button = Button(image=right_img, highlightthickness=0, command=known)
+right_button.grid(row=1, column=1)
 
-password_label = Label(text="Password :")
-password_label.grid(column=0, row=3)
-
-# Input User Data
-website_entry = Entry(width=21)
-website_entry.grid(column=1, row=1)
-website_entry.focus()
-
-# Search Button
-search = Button(text='Search', command=find_password)
-search.grid(column=2, row=1)
-
-email_entry = Entry(width=35)
-email_entry.grid(column=1, row=2, columnspan=2)
-email_entry.insert(0, 'rishabh@gmail.com')
-
-password_entry = Entry(width=21)
-password_entry.grid(column=1, row=3)
-
-# Creating Button
-generate_password = Button(text='Generate', command=generate_password)
-generate_password.grid(column=2, row=3)
-
-add = Button(text='Add', width=36, command=save)
-add.grid(column=1, row=4, columnspan=2)
+generate_random_word()
 window.mainloop()
